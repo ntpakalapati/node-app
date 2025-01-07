464a78c43638b59f9850a682e4925db091ab0034
ntpakalapati <nvdp.pakalapati@gmail.com>
2025-01-07 23:12:02 +0530

    mariandb

--- api/controllers/rolesController.js
+++ api/controllers/rolesController.js
@@ -1,6 +1,4 @@
-const { asyncFunction } =require("../service/connection");
-
-// import { asyncFunction } from "../service/connection";
+const { asyncFunction } = require("../service/connection");
 
 let roles = [
   { role_id: 1, institution_id: 1, role_name: "admin", status: "Active" },
@@ -8,13 +6,14 @@ let roles = [
 ];
 
 const getAllRoles = async (req, res) => {
-  try{
-    const data = await asyncFunction()
-    const roledata = data ? data : roles
-    res.json(roledata);
-  } catch {
-    res.json({error: "roles api"})
-  }
+  const data = await asyncFunction();
+  const roledata = data ? data : roles;
+  const bigIntReplacer = (key, value) => 
+    typeof value === 'bigint' ? value.toString() : value;
+
+  res.json({
+    data: JSON.parse(JSON.stringify(roledata, bigIntReplacer))
+  });
 };
 
-module.exports = { getAllRoles }
+module.exports = { getAllRoles };
--- api/index.js
+++ api/index.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser');
 var app = express();
 var cors = require('cors');
 const  rolesRoutes  = require("./routes/rolesRoute")
-const { createUser, getUserByEmail, getProducts, insertProduct } = require('./service/utils');
+// const { createUser, getUserByEmail, getProducts, insertProduct } = require('./service/utils');
 app.use(bodyParser.json());
 app.use(cors())
 app.listen(8000);
@@ -12,20 +12,20 @@ app.get('/', function (req, res) {
   res.send('node js');
 })
 
-app.use("/api", rolesRoutes);
+app.use(rolesRoutes);
 
-app.post('/register', function (req, res) {
-  return createUser(req, res);
-}) 
+// app.post('/register', function (req, res) {
+//   return createUser(req, res);
+// }) 
 
-app.get('/login', function (req, res) {
-  return getUserByEmail(req, res)
-});
+// app.get('/login', function (req, res) {
+//   return getUserByEmail(req, res)
+// });
 
-app.get('/products', function (req, res) {
-  return getProducts(req, res)
-});
+// app.get('/products', function (req, res) {
+//   return getProducts(req, res)
+// });
 
-app.post('/addproduct', function(req,res) {
-  return insertProduct(req,res)
-})
+// app.post('/addproduct', function(req,res) {
+//   return insertProduct(req,res)
+// })
